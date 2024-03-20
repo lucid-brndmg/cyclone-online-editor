a11a7943f4bfcdbeaea2a22a158e651aa5d0792e
Vanush Misha Paturyan <misha@cs.nuim.ie>
2024-03-20 01:16:01 +0000

    Docker Compose configuration for production test
    Frontend working, backend not yet linked

--- next.config.js
+++ next.config.js
@@ -6,8 +6,12 @@ const monacoRules = [
   }
 ]
 /** @type {import('next').NextConfig} */
+
+const baseUrl = process.env.PUBLIC_URL || ''
+
 const nextConfig = {
   reactStrictMode: true,
+  basePath: baseUrl,
   webpack: (config, {isServer}) => {
     if(!isServer) {
       config.plugins.push(new MonacoWebpackPlugin({
