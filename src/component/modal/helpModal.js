cb5db77d26c23466e83077aec0f7c7415e5f336a
Haoyang Lu <haoyang.lu.2024@mumail.ie>
2024-04-14 03:53:01 +0100

    Help text

--- src/component/modal/helpModal.js
+++ src/component/modal/helpModal.js
@@ -19,18 +19,17 @@ import {PublicUrl} from "@/core/utils/resource";
 export const HelpModal = ({opened, onOpened}) => {
   const editorItems = [
     {icon: IconFolder, bold: "File Browser", text: "Store all edited cyclone source code in the browser. Also access all official examples instantly."},
-    {icon: IconBook2, bold: "Reference", text: "Look up language keywords, build-in functions & operators, compile options instantly."},
-    {icon: IconListTree, bold: "Outline", text: "View code structure in a tree and jump to definition in one click just like VSCode. Also view all errors & warnings inside."},
-    {icon: IconEye, bold: "Visualization", text: "Preview cyclone's node & edge defined in the source code as graph in real-time. View execution result & trace as images."}
+    {icon: IconBook2, bold: "Reference", text: "Look up language keywords, build-in functions, operators and compile options instantly."},
+    {icon: IconListTree, bold: "Outline", text: "View code structure in a tree and jump to definition in one click just like VSCode. Navigate all errors & warnings inside source code."},
+    {icon: IconEye, bold: "Visualization", text: "Preview a Cyclone program as image in real-time. Graphically observe code execution result & trace."}
   ]
 
   const codeEditorItems = [
     {bold: "Syntax Highlighting", text: "Display highlighted source code and supports multiple themes."},
     {bold: "Syntactic Analysis", text: "Check cyclone source code by syntax. Errors will be highlighted."},
-    // {bold: "Type Checking", text: "Check existing type errors (including self-defined functions & built-in operators) inside source code."},
-    {bold: "Semantic Analysis", text: "Check all kinds of semantic errors (such as type errors, using undefined variables, invalid start node or unreachable code) inside source code"},
-    {bold: "Hover Information", text: "Display type info & reference docs (for built-in functions & keywords) on hover"},
-    {bold: "Code Completion & Snippets", text: "Provide auto-completions & snippets when typing. Scoped identifiers could be provided as well."},
+    {bold: "Semantic Analysis", text: "Check semantic errors inside source code"},
+    {bold: "Quick Documentations", text: "Display type info & reference docs (for built-in functions & keywords) on hover"},
+    {bold: "Code Completion & Snippets", text: "Provide auto-completions & snippets while typing."},
   ]
 
   const close = () => onOpened(false)
@@ -41,7 +40,7 @@ export const HelpModal = ({opened, onOpened}) => {
         <Stack>
           <SimpleGrid cols={2}>
             <Stack gap={"xs"}>
-              <Text fw={500}>The Code Editor</Text>
+              <Text fw={500}>Code Editor</Text>
               <Text size={"sm"}>
                 The code editor provides the ability to check the code both by syntax and semantic. Errors and warnings will be explicitly displayed on the editor. Full features:
               </Text>
@@ -61,11 +60,11 @@ export const HelpModal = ({opened, onOpened}) => {
               </List>
               <Space />
               <Text size={"sm"}>
-                Lite version of the code editor is also available when browsing tutorial but with less functionalities. To unlock the full power of the cyclone editor and get the best user experience, please visit <Anchor href={PublicUrl.Editor}>Cyclone Editor</Anchor>.
+                Lite version of the code editor is available when <Anchor href={PublicUrl.Tutorial}>browsing tutorial</Anchor>. To unlock the full power of the cyclone editor and get the best user experience, please visit <Anchor href={PublicUrl.Editor}>Cyclone Editor</Anchor>.
               </Text>
             </Stack>
             <Stack gap={"xs"}>
-              <Text fw={500}>The Cyclone Editor</Text>
+              <Text fw={500}>Cyclone Editor</Text>
               <Text size={"sm"}>
                 The Cyclone Editor unlocks the full power of editing Cyclone by providing:
               </Text>
@@ -92,9 +91,6 @@ export const HelpModal = ({opened, onOpened}) => {
               </List>
               <Space />
               <Text size={"sm"}>
-                Try to open visualization panel when typing, it will be instantly updated and you may see how your code was structured using graph representation.
-              </Text>
-              <Text size={"sm"}>
                 When clicking "Run" button at the top, the code will be executed by Cyclone and the result will be displayed as text at "Execution Result" panel. If currently inside the cyclone editor, visualization representation of the result & trace will be ready at "Visualization" panel.
               </Text>
             </Stack>
@@ -102,7 +98,8 @@ export const HelpModal = ({opened, onOpened}) => {
           <Stack gap={"xs"}>
             <Text fw={500}>About</Text>
             <Text size={"sm"}>
-              The Cyclone language is written by <Anchor href={"https://classicwuhao.github.io/"}>Hao Wu</Anchor>. Cyclone online editor is a final year project written by <Anchor href={"https://github.com/lucid-brndmg"}>Haoyang Lu</Anchor> under Dr. Hao Wu's guidance at Maynooth University.
+              Cyclone is designed by <Anchor  href={"https://classicwuhao.github.io/"}>Hao Wu</Anchor>.
+              This website is designed by <Anchor  href={"https://github.com/lucid-brndmg"}>Haoyang Lu</Anchor>.
             </Text>
             <Group justify={"space-between"}>
               <Anchor fz={"sm"} href={"https://classicwuhao.github.io/cyclone_tutorial/expr/reference.html"}>Language Reference</Anchor>
